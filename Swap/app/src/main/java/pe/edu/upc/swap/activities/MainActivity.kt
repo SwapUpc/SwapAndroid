eff0f52cae18139437adcbee195089dfc023ada4
Pieter Gleixner <pjgleixner@gmail.com>
2019-09-23 18:12:07 -0500

    Architecture

---
+++ Swap/app/src/main/java/pe/edu/upc/swap/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package pe.edu.upc.swap.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.upc.swap.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Swap/app/src/main/java/pe/edu/upc/swap/data/network/model/ApiResponse.kt
@@ -0,0 +1,4 @@
+package pe.edu.upc.swap.data.network.model
+
+class ApiResponse {
+}
